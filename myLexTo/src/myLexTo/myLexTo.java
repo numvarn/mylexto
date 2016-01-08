26be4bd37f70641a4badab7863f1090a4a7ba850
Phisan Sookkhee <phisan@Phisans-MacBook-Pro.local>
2016-01-08 15:57:25 +0700

    modified:   LexTo/build/built-jar.properties 	deleted:    LexTo/build/classes/.netbeans_automatic_build 	deleted:    LexTo/build/classes/.netbeans_update_resources 	modified:   LexTo/build/classes/LexTo/LongLexTo.class 	modified:   LexTo/build/classes/LexTo/LongParseTree.class 	modified:   LexTo/build/classes/LexTo/Trie.class 	modified:   LexTo/dist/LexTo.jar 	modified:   LexTo/src/LexTo/LongLexTo.java 	modified:   myLexTo/build/built-jar.properties 	modified:   myLexTo/build/classes/myLexTo/myLexTo.class 	modified:   myLexTo/dist/lib/LexTo.jar 	modified:   myLexTo/dist/myLexTo.jar 	modified:   myLexTo/src/myLexTo/myLexTo.java

--- myLexTo/src/myLexTo/myLexTo.java
+++ myLexTo/src/myLexTo/myLexTo.java
@@ -11,71 +11,130 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Vector;
 
 /**
  *
  * @author phisan
  */
 public class myLexTo {
-
+    
     public static void main(String args[]) throws IOException {
-        LongLexTo tokenizer = new LongLexTo(new File("/Users/phisan/ResearchCode/thaiword/myLexTo/src/myLexTo/lexitron.txt"));
-        File unknownFile = new File("/Users/phisan/ResearchCode/thaiword/myLexTo/src/myLexTo/unknown.txt");
-        if (unknownFile.exists()) {
-            tokenizer.addDict(unknownFile);
-        }
+        if (args.length > 0) {
+            String baseDirectory = args[0];
+            String targetDirectory = baseDirectory + "/lexto";
+            String textDirectory = targetDirectory + "/text";
+            String htmlDirectory = targetDirectory + "/html";
+            String line;
+            
+            int begin, end, type;
+            String [] nameTokens;
 
-        String line;
-        int begin, end;
+            File inFile, outFile, outFileHTML;
+            FileReader fr;
+            BufferedReader br;
+            FileWriter fw, fwhtml;
+            Vector typeList;
 
-        File inFile, outFile;
-        FileReader fr;
-        BufferedReader br;
-        FileWriter fw;
-
-        String baseDirectory = "/Users/phisan/Desktop/Test";
-        String targetDirectory = baseDirectory+"/lexto";
-
-        File theDir = new File(targetDirectory);
-        if (!theDir.exists()) {
-            try {
-                theDir.mkdir();
-            } catch (SecurityException se) {
-                System.out.println(se);
-            }
-        }
-
-        final File folder = new File(baseDirectory);
-        for (final File fileEntry : folder.listFiles()) {
-            if (!fileEntry.isDirectory()) {
-                System.out.println("Processing File : " + fileEntry.getName());
-                inFile = new File(baseDirectory + "//" + fileEntry.getName());
-                outFile = new File(targetDirectory + "//"+fileEntry.getName());
-
-                fr = new FileReader(inFile);
-                br = new BufferedReader(fr);
-                fw = new FileWriter(outFile);
-
-                while ((line = br.readLine()) != null) {
-                    line = line.trim();
-                    if (line.length() > 0) {
-                        tokenizer.wordInstance(line);
-                        begin = tokenizer.first();
-                        int i = 0;
-                        while (tokenizer.hasNext()) {
-                            end = tokenizer.next();
-                            fw.write(line.substring(begin, end));
-                            fw.write("|");
-                            begin = end;
-                        }
-                        fw.write(System.lineSeparator());
-                        fw.write(System.lineSeparator());
-                    }
+            File theDir = new File(targetDirectory);
+            if (!theDir.exists()) {
+                try {
+                    theDir.mkdir();
+                } catch (SecurityException se) {
+                    System.out.println(se);
                 }
-                
-                fr.close();
-                fw.close();
             }
+
+            File textDir = new File(textDirectory);
+            if (!textDir.exists()) {
+                try {
+                    textDir.mkdir();
+                } catch (SecurityException se) {
+                    System.out.println(se);
+                }
+            }
+
+            File htmlDir = new File(htmlDirectory);
+            if (!htmlDir.exists()) {
+                try {
+                    htmlDir.mkdir();
+                } catch (SecurityException se) {
+                    System.out.println(se);
+                }
+            }
+
+            /* Create LexTo instance*/
+            LongLexTo tokenizer = new LongLexTo(new File("/Users/phisan/ResearchCode/thaiword/myLexTo/src/myLexTo/lexitron.txt"));
+            File unknownFile = new File("/Users/phisan/ResearchCode/thaiword/myLexTo/src/myLexTo/unknown.txt");
+            if (unknownFile.exists()) {
+                tokenizer.addDict(unknownFile);
+            }
+
+            final File folder = new File(baseDirectory);
+            int count = 1;
+            for (final File fileEntry : folder.listFiles()) {
+                if (!fileEntry.isDirectory()) {
+                    System.out.println(count + " : Processed File -- " + fileEntry.getName());
+                    
+                    nameTokens = fileEntry.getName().split("\\.(?=[^\\.]+$)");
+                    
+                    inFile = new File(baseDirectory + "//" + fileEntry.getName());
+                    outFile = new File(textDirectory + "//" + fileEntry.getName());
+                    outFileHTML = new File(htmlDirectory + "//" + nameTokens[0]+".html");
+
+                    fr = new FileReader(inFile);
+                    br = new BufferedReader(fr);
+                    fw = new FileWriter(outFile);
+                    fwhtml = new FileWriter(outFileHTML);
+
+                    while ((line = br.readLine()) != null) {
+                        line = line.trim();
+                        if (line.length() > 0) {
+                            tokenizer.wordInstance(line);
+                            typeList = tokenizer.getTypeList();
+                            
+                            begin = tokenizer.first();
+                            int i = 0;
+                            while (tokenizer.hasNext()) {
+                                end = tokenizer.next();
+                                
+                                /* write to Text file */
+                                fw.write(line.substring(begin, end));
+                                fw.write("|");
+
+                                /* write HTML file*/
+                                type = ((Integer) typeList.elementAt(i++)).intValue();
+                                if (type == 0) {
+                                    fwhtml.write("<font color=#ff0000>" + line.substring(begin, end) + "</font>");
+                                } else if (type == 1) {
+                                    fwhtml.write("<font color=#00bb00>" + line.substring(begin, end) + "</font>");
+                                } else if (type == 2) {
+                                    fwhtml.write("<font color=#0000bb>" + line.substring(begin, end) + "</font>");
+                                } else if (type == 3) {
+                                    fwhtml.write("<font color=#aa00aa>" + line.substring(begin, end) + "</font>");
+                                } else if (type == 4) {
+                                    fwhtml.write("<font color=#00aaaa>" + line.substring(begin, end) + "</font>");
+                                }
+                                fwhtml.write("<font color=#000000>|</font>");
+                                
+                                begin = end;
+                            }
+                            fw.write(System.lineSeparator());
+                            fw.write(System.lineSeparator());
+                            
+                            fwhtml.write("<br>\n");
+                        }
+                    }
+
+                    fr.close();
+                    fw.close();
+                    fwhtml.close();
+                    count++;
+                }
+            }
+        } else {
+            System.out.println("Please, Enter file path.");
+            System.exit(0);
         }
     }
 }
